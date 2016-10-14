be6729e6a8c03c2541d6e66ba5db917ef4c6bf89
Carlos Cuatin <cecg1996@gmail.com>
2016-10-13 23:16:15 -0500

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+const routes = () =>
+  <Route path="/" component={() => <div>Start from here!</div>} />;
+
+export default routes;
