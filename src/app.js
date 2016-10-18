ef2c3025d7bb5bda7343f1643338fa5cfb7184e7
Carlos Cuatin <cecg1996@gmail.com>
2016-10-17 23:46:20 -0500

    update initial config

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import Provider from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from 'routes';
+import rootSaga from 'sagas';
+import configureStore from 'store/configureStore';
+
+const store = configureStore();
+store.runSaga(rootSaga);
 
 const App = () => (
-  <Router history={browserHistory}>
-    {routes()}
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      {routes()}
+    </Router>
+  </Provider>
 );
 
 render(<App />, document.getElementById('root'));
